dc7b1d331df2a50ae711ac2919f4835cb060c457
Gduregon <gjd9961@nyu.edu>
2022-11-28 17:11:59 -0500

    Changing names, added context manager for matmul dtype in greedytrain

--- LowPrecisionApproxGP/util/GreedyTrain.py
+++ LowPrecisionApproxGP/util/GreedyTrain.py
@@ -1,4 +1,3 @@
-from audioop import maxpp
 from datetime import date
 from typing import Tuple
 import torch
@@ -9,6 +8,7 @@ import numpy as np
 from GreedySelector import greedy_select_points
 import os
 from datetime import date
+import gpytorch
 
 experiment_folder_path = os.getenv("EXPERIMENT_OUTPUTS")
 if experiment_folder_path is None:
@@ -49,56 +49,60 @@ def greedy_train(
         {max_inducing_points} max inducing points"
     )
 
-    for i in range(max_iter):
-        # If haven't gotten any inducing points, grab a random one
-        if len(inducing_point_candidates) == len(train_x):
-            random_index = np.random.randint(0, len(train_x))
-            first_inducing_point = (
-                inducing_point_candidates[random_index].detach().clone().reshape(1, -1)
-            )  # Get
-            model.covar_module.inducing_points = torch.nn.Parameter(
-                first_inducing_point, requires_grad=False
-            )  # Set
-            # Remove Selected Point from candidate set
-            inducing_point_candidates = torch.cat(
-                (
-                    inducing_point_candidates[:random_index],
-                    inducing_point_candidates[random_index + 1 :],
-                ),
-                dim=0,
-            )
+    with gpytorch.settings.linalg_dtypes(default=dtype):
+        for i in range(max_iter):
+            # If haven't gotten any inducing points, grab a random one
+            if len(inducing_point_candidates) == len(train_x):
+                random_index = np.random.randint(0, len(train_x))
+                first_inducing_point = (
+                    inducing_point_candidates[random_index]
+                    .detach()
+                    .clone()
+                    .reshape(1, -1)
+                )  # Get
+                model.covar_module.inducing_points = torch.nn.Parameter(
+                    first_inducing_point, requires_grad=False
+                )  # Set
+                # Remove Selected Point from candidate set
+                inducing_point_candidates = torch.cat(
+                    (
+                        inducing_point_candidates[:random_index],
+                        inducing_point_candidates[random_index + 1 :],
+                    ),
+                    dim=0,
+                )
 
-        elif len(model.covar_module.inducing_points) >= max_inducing_points:
-            logging.info(
-                f"Model : {model_name}, Message : Breaking out of training loop at iteration {i+1}/{max_iter}. Reached limit of inducing points: we have {len(model.covar_module.inducing_points)} \
-                points with a maximum of {max_inducing_points}"
-            )
-            break
-        else:
-            inducing_point_candidates = greedy_select_points(
-                model, inducing_point_candidates, train_x, train_y, mll
-            )
-            if inducing_point_candidates is None:
-                # We've failed to find a point that increases our Likelihood
+            elif len(model.covar_module.inducing_points) >= max_inducing_points:
                 logging.info(
-                    f"Model : {model_name}, Message : Breaking out of training loop at iteration {i}/{max_iter}. Failed to add inducing point."
+                    f"Model : {model_name}, Message : Breaking out of training loop at iteration {i+1}/{max_iter}. Reached limit of inducing points: we have {len(model.covar_module.inducing_points)} \
+                    points with a maximum of {max_inducing_points}"
                 )
                 break
+            else:
+                inducing_point_candidates = greedy_select_points(
+                    model, inducing_point_candidates, train_x, train_y, mll
+                )
+                if inducing_point_candidates is None:
+                    # We've failed to find a point that increases our Likelihood
+                    logging.info(
+                        f"Model : {model_name}, Message : Breaking out of training loop at iteration {i}/{max_iter}. Failed to add inducing point."
+                    )
+                    break
 
-        # Zero gradients from previous iteration
-        optimizer.zero_grad()
-        mll.zero_grad()
-        # Output from model
-        output = model(train_x)
-        # Calc average loss and backprop gradients
-        loss = -mll(output, train_y)
-        loss.mean().backward()
+            # Zero gradients from previous iteration
+            optimizer.zero_grad()
+            mll.zero_grad()
+            # Output from model
+            output = model(train_x)
+            # Calc average loss and backprop gradients
+            loss = -mll(output, train_y)
+            loss.mean().backward()
 
-        logging.info(
-            f"Model : {model_name}, Message : Iteration: {i+1}/{max_iter} - Average Loss: {loss.mean().item()}"
-        )
-        torch.cuda.empty_cache()
+            logging.info(
+                f"Model : {model_name}, Message : Iteration: {i+1}/{max_iter} - Average Loss: {loss.mean().item()}"
+            )
+            torch.cuda.empty_cache()
 
-        optimizer.step()
+            optimizer.step()
 
     return model
