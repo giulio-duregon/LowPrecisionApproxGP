3e3b081eda96e57e7ac3d5fbf00fe11da0ab8e10
Gduregon <gjd9961@nyu.edu>
2022-12-05 19:38:12 -0500

    Adding helper script for experimentation

---
+++ scripts/run_experiments.sh
@@ -0,0 +1,11 @@
+# For data set bikes, run with base kernel
+# python model_runner.py -d bikes -l true -dt double -ip 50 -it 50 -dt double -m true -j 10
+# python model_runner.py -d bikes -l true -dt double -ip 50 -it 50 -dt single -m true -j 10
+# python model_runner.py -d bikes -l true -dt double -ip 100 -it 100 -dt double -m true -j 10
+# python model_runner.py -d bikes -l true -dt double -ip 100 -it 100 -dt single -m true -j 10
+# python model_runner.py -d bikes -l true -dt double -ip 200 -it 200 -dt double -m true -j 10
+# python model_runner.py -d bikes -l true -dt double -ip 200 -it 200 -dt single -m true -j 10
+# python model_runner.py -d bikes -l true -dt double -ip 500 -it 500 -dt double -m true -j 10
+# python model_runner.py -d bikes -l true -dt double -ip 500 -it 500 -dt single -m true -j 10
+
+python model_runner.py -d bikes -l true -dt double -ip 100 -it 100 -dt single
