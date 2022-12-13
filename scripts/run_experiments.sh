12bb738bf88cff66fe208c4c45582c4a650f22b6
Gduregon <gjd9961@nyu.edu>
2022-12-13 02:24:16 -0500

    Adding latest run_experiments file

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -1,30 +1,41 @@
-# For data set bikes, run with base kernel
-
-for dataset in "energy" "bikes" "protein" "naval"; do
-    echo $dataset
-    python model_runner.py -d $dataset -l true -ip 50 -it 50 -dt single
-    python model_runner.py -d $dataset -l true -ip 50 -it 50 -dt double
+# Run experiments on small datasets with first greedy
+for dataset in "bikes" "protein" "naval" "energy"; do
+    for ip in 50 75 100 150 200; do
+        for precision in "single" "double"; do
+            echo $dataset, $ip, $precision
+            python model_runner.py -d $dataset -l true -ip $ip -it $ip -dt $precision
+        done
+    done
 done
 
-# for dataset in "bikes" "protein" "naval" "road3d"; do
-#     for ip in 50 75 100 150 200; do
-#         for precision in "single" "double"; do
-#             echo $dataset, $ip, $precision
-#             python model_runner.py -d $dataset -l true -ip $ip -it $ip -dt $precision
-#         done
-#     done
-# done
+# Run experiments on small datasets with max greedy
+for dataset in "bikes" "protein" "naval" "energy"; do
+    for ip in 50 75 100 150 200; do
+        for precision in "single" "double"; do
+            for j in 10 20 40 60; do
+                echo $dataset, $ip, $precision, $j
+                python model_runner.py -d $dataset -l true -ip $ip -it $ip -dt $precision -m true -j $j -mj 10
+            done
+        done
+    done
+done
 
-# for dataset in "bikes" "protein" "naval" "road3d"; do
-#     for ip in 50 75 100 150 200; do
-#         for precision in "single" "double"; do
-#             for j in 10 20 50 100; do
-#                 echo $dataset, $ip, $precision, $j
-#                 python model_runner.py -d $dataset -l true -ip $ip -it $ip -dt $precision -m true -j $j -mj 10
-#             done
-#         done
-#     done
-# done
+# road3d last for both max/first greedy since it takes forever
+for ip in 50 75 100 150 200; do
+    for precision in "single" "double"; do
+        echo "road3d", $ip, $precision
+        python model_runner.py -d road3d -l true -ip $ip -it $ip -dt $precision
+    done
+done
+
+for ip in 50 75 100 150 200; do
+    for precision in "single" "double"; do
+        for j in 10 20 40 60; do
+            echo "road3d" $ip, $precision, $j
+            python model_runner.py -d road3d -l true -ip $ip -it $ip -dt $precision -m true -j $j -mj 10
+        done
+    done
+done
 
 # Parse the output logs
 echo "Parsing logs!"
